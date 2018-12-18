b7b40ae60a7482989774605b277a87a8a4167b34
Daniel Huang <dhuang684@gmail.com>
2018-12-18 12:56:02 -0500

    added functionality to redraw grid
    need to be able to clear grid.

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 //try flexbox
 /*
-to add a new class name to a div
-document.getElementById("div").classList.add('colorChange');
+adding a button and having the submit grab the value for the backend function
 
 */
+document.addEventListener("mouseover",(e)=>{
+  if(!e.target.classList.contains("box")){
+      return;
+  }
+  e.target.style.background = "#3d3d3d";
+})
+
 //create a grid
-function grid(el) {
-  var container = document.createElement("div");
-  container.id="main";
-  container.className = "container";
-  
-  for (i = 0; i <= 16; i++){
-    var row = document.createElement("div");
-    row.className = "row";
-    row.id = "row" + i;
+//generates a grid with the original div container already on index.html you need to start messing with the code you work with to gain a better understanding of what it's doing
+function grid() {
+  var container = document.getElementById("container");
+      for (i = 0; i <= 16; i++){
+          var row = document.createElement("div");
+              row.className = "row";
+              row.id = "row" + i;
     
-    for(k = 0; k <= 16; k++){
-      var box = document.createElement("div");
-      box.className = "box";
-      row.appendChild(box);
-    }
-    container.appendChild(row);
+     for(k = 0; k <= 16; k ++){
+          var box = document.createElement("div");
+           box.className = "box";
+           row.appendChild(box);
+          }
+           container.appendChild(row);
+          }
+           return container;
+        }
+          grid(document.body);
+            
+
+
+
+function resizeGrid(){
+  clearGrid();
+  //missing a clear grid function!
+  var resize = document.getElementById('redrawGrid').value;
+  var container = document.getElementById("container");
+      for (i = 0; i <= resize; i++){
+          var row = document.createElement("div");
+              row.className = "row";
+              row.id = "row" + i;
+    
+     for(k = 0; k <= resize; k ++){
+          var box = document.createElement("div");
+           box.className = "box";
+           row.appendChild(box);
+          }
+           container.appendChild(row);
+          }
+           return container;
+          }
+  
+  //right now the resizing is adding to the bottom of the old grid.
+  function clearGrid(){
     
   }
-  el.appendChild(container);
-}
-grid(document.body);
-
-
-
 
 /*
 //functional code, just going to stick to event delegation.
@@ -46,12 +74,7 @@ function changeColor() {
 
 */
 //google event delegation
-document.addEventListener("mouseover",(e)=>{
-  if(!e.target.classList.contains("box")){
-      return;
-  }
-  e.target.style.background = "#3d3d3d";
-})
+
 
 /*
 
